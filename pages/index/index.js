a8d7aa4eee5d172b5054b85826d98704f9914c72
unknown <unknown@example.com>
2020-05-23 20:22:20 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,36 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  examine:function(){
+    wx.navigateTo({
+      url: '/pages/examine/examine',
+    })
+  },
+  diary:function(){
+    wx.navigateTo({
+      url: '/pages/index/diary',
+    })
+  },
+  drugs:function(){
+    wx.navigateTo({
+      url: '/pages/drugs/drugs',
+    })
+  },
+  chineseHeal:function(){
+    wx.navigateTo({
+      url: '/pages/chineseHealth/index',
+    })
+  },
+  askDoctor:function(){
+    wx.navigateTo({
+      url: '/pages/askDoctor/index',
+    })
+  },
+  recommend:function(){
+    wx.navigateTo({
+      url: '/pages/index/recommend',
+    })
+  }
+})
